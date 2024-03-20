2452922cc934248e83f36e0b8d2a6cf7debc2dfe
Munupixi <driverufa12@gmail.com>
2024-03-20 12:41:17 +0500

    changed profile

--- ProfilePage.xaml.cs
+++ ProfilePage.xaml.cs
@@ -20,11 +20,36 @@ namespace TimeCraft
         public ProfilePage()
         {
             InitializeComponent();
+            NameTextBox.Text = User.ActiveUser.Name;
+            SurnameTextBox.Text = User.ActiveUser.Surname;
+            PatronymicTextBox.Text = User.ActiveUser.Patronymic;
+            PasswordTextBox.Text = User.ActiveUser.Password;
+            AgeTextBox.Text = User.ActiveUser.Age.ToString();
+            LoginTextBox.Text = User.ActiveUser.Login;
+        }
+        private bool IsAllCorrect()
+        {
+            return (!string.IsNullOrEmpty(NameTextBox.Text) &&
+                User.IsAgeCorrect(AgeTextBox.Text) &&
+                User.IsLoginCorrect(LoginTextBox.Text) &&
+                User.IsPasswordCorrect(PasswordTextBox.Text) &&
+                PasswordTextBox.Text == PasswordAgainTextBox.Text);
         }
 
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!IsAllCorrect())
+            {
+                MessageBox.Show("Не все поля заполнены корректно");
+                return;
+            }
+            User.ActiveUser.Name = NameTextBox.Text;
+            User.ActiveUser.Surname = SurnameTextBox.Text;
+            User.ActiveUser.Patronymic = PatronymicTextBox.Text;
+            User.ActiveUser.Password = PasswordTextBox.Text;
+            User.ActiveUser.Age = Convert.ToInt32(AgeTextBox.Text);
+            User.ActiveUser.Update();
+            NavigationService.Navigate(new WeeklySchedule());
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
