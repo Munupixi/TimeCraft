be9953c39ec00968d0c3e7979f599489fd43f076
bikti <biktimerovff2005@gmail.com>
2024-10-29 12:10:07 +0500

    change password for database for me

--- Data/DataBaseContent.cs
+++ Data/DataBaseContent.cs
@@ -53,7 +53,7 @@ namespace TimeCraft
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TimeCraft;Username=postgres;Password=123");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TimeCraft;Username=postgres;Password=Faza2005");
             //optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=TimeCraft;Username=postgres;Password=Faza2005");
 
             //var config = new ConfigurationBuilder()
